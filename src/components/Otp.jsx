12a368e1ca8a48ae18a0d7d67729399f44bf32f6
sabbir-sabbir <iosabbir7890@gmail.com>
2024-10-20 23:13:02 +0600

    new commit

--- src/components/Otp.jsx
+++ src/components/Otp.jsx
@@ -3,12 +3,23 @@ import  { useState } from 'react'
 
 export default function Otp({ otpLength = 6 }) {
     const [otpFields, setOtpFields] = useState(new Array(otpLength).fill(""))
-    console.log(otpFields);
+    const HandleKeyDown = (e) => {
+        const key = e.key 
+        if(isNaN(key)) {
+            return; 
+        }
+        console.log(key);
+        const copyOtpFields = [...otpFields];
+        copyOtpFields[index] = key;
+
+
+    };
   return (
     <>
     <div className="container">
         {otpFields.map((value, index ) => {
-            return <input key={index} type="text" value={value} />;
+            return <input key={index} type="text" value={value} onKeyDown={(e)=>HandleKeyDown(e,index)} />;
+
         })
 
         }
